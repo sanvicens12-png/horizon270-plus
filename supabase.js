7e6ef5e12c9046e6c2b086a8a27be1ace9ab1f7d
sanvicens12-png <sanvicens12@gmail.com>
2026-08-18 16:08:07 +0200

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://eftxwdpdweomffgurlgm.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://eftxwdpdweomffgurlgm.supabase.co";
 const SUPABASE_KEY = "sb_publishable_DPCJgsTp66cw2s-5S3CqNg_AdJEHKpt";
 
 const supabaseClient = window.supabase.createClient(
