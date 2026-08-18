dd716e2fb28ba84d3f3f4386698ded0c1b12f94e
sanvicens12-png <sanvicens12@gmail.com>
2026-08-18 15:50:34 +0200

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "POSA_AQUI_L_URL_DE_L_API";
+const SUPABASE_KEY = "POSA_AQUI_LA_CLAU_PUBLICABLE";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
