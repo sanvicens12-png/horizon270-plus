a2ae479d600e9ed7f00ea07ec0e460e71fd70729
sanvicens12-png <sanvicens12@gmail.com>
2026-08-18 16:13:31 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://eftxwdpdweomffgurlgm.supabase.co";
-const SUPABASE_KEY = "sb_publishable_DPCJgsTp66cw2s-5S3CqNg_AdJEHKpt";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
